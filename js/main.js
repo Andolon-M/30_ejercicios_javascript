0c61ffa4373e7bc74492fdb4760fe85bff63eeaf
Andolon-M <aemendez-l@outlook.com>
2024-05-24 15:38:10 -0500

    feat: :tada: Se Actualizaron los puertos y las librerias para el despliegue en el servidor.

--- js/main.js
+++ js/main.js
@@ -1,56 +1,56 @@
-import {
-getAllOfficeCodeCiyt,
-getAllOfficeCityAnsMovil
-} from "./module/offices.js";
+// import {
+// getAllOfficeCodeCiyt,
+// getAllOfficeCityAnsMovil
+// } from "./module/offices.js";
 
-import {
-    getFullNameAndEmails,
-    getAllEmployess,
-    getComapnyBoos
-} from "./module/employess.js";
+// import {
+//     getFullNameAndEmails,
+//     getAllEmployess,
+//     getComapnyBoos
+// } from "./module/employess.js";
 
-import {
-    getAll,
-    getPaymentsByParameter
-} from "./module/payments.js";
+// import {
+//     getAll,
+//     getPaymentsByParameter
+// } from "./module/payments.js";
 
-import {
-    getAllClients,
-    getNameClientsWitchNameSalesManager
-}from "./module/clients.js";
+// import {
+//     getAllClients,
+//     getNameClientsWitchNameSalesManager
+// }from "./module/clients.js";
 
-//1. Devuelve un listado con el código de oficina y la ciudad donde hay oficinas.
-//console.log(await getAllOfficeCodeCiyt());
-
-
-//2. Devuelve un listado con la ciudad y el teléfono de las oficinas de España.
-//console.log(await getAllOfficeCityAnsMovil())
+// 1. Devuelve un listado con el código de oficina y la ciudad donde hay oficinas.
+// console.log(await getAllOfficeCodeCiyt());
 
 
-//3. Devuelve un listado con el nombre, apellidos y email de los empleados cuyo jefe tiene un código de jefe igual a 7.
-//console.log(await getFullNameAndEmails());
+// 2. Devuelve un listado con la ciudad y el teléfono de las oficinas de España.
+// console.log(await getAllOfficeCityAnsMovil())
 
 
-//4. Devuelve el nombre del puesto, nombre, apellidos y email del jefe de la empresa.
-//let data = await getAllEmployess();
-//console.log(await getComapnyBoos(data))
+// 3. Devuelve un listado con el nombre, apellidos y email de los empleados cuyo jefe tiene un código de jefe igual a 7.
+// console.log(await getFullNameAndEmails());
 
 
-/*
-8. Devuelve un listado con el código de cliente de aquellos clientes que realizaron algún pago en 2008. 
-Tenga en cuenta que deberá eliminar aquellos códigos de cliente que aparezcan repetidos. Resuelva la consulta:
-*/
-//let data = await getAll();
-//let dataWitFilterByParameter_1 = await getPaymentsByParameter(data, "payment", "PayPal");
-//let dataWitFilterByParameter_2 = await getPaymentsByParameter(dataWitFilterByParameter_1, "year", "2008");
-//console.log(dataWitFilterByParameter_2);
+// 4. Devuelve el nombre del puesto, nombre, apellidos y email del jefe de la empresa.
+// let data = await getAllEmployess();
+// console.log(await getComapnyBoos(data))
 
 
 
-/*
-1.2 1. Obtén un listado con el nombre de cada cliente y el nombre y apellido de su
-representante de ventas.
-*/
+// 8. Devuelve un listado con el código de cliente de aquellos clientes que realizaron algún pago en 2008. 
+// Tenga en cuenta que deberá eliminar aquellos códigos de cliente que aparezcan repetidos. Resuelva la consulta:
 
-let data = await getAllClients();
-console.log (await getNameClientsWitchNameSalesManager(data));
+// let data = await getAll();
+// let dataWitFilterByParameter_1 = await getPaymentsByParameter(data, "payment", "PayPal");
+// let dataWitFilterByParameter_2 = await getPaymentsByParameter(dataWitFilterByParameter_1, "year", "2008");
+// console.log(dataWitFilterByParameter_2);
+
+
+
+
+// 1.2 1. Obtén un listado con el nombre de cada cliente y el nombre y apellido de su
+// representante de ventas.
+
+
+// let data = await getAllClients();
+// console.log (await getNameClientsWitchNameSalesManager(data));
